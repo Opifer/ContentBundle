7a4df489a48021ebbd15102d7fdde5ab40b98fe4
Michael Sieben <m.sieben@opifer.nl>
2015-02-24 11:09:28 +0100

    Removed unnecessary import. Changed confirmation button text.

--- Controller/Api/ContentController.php
+++ Controller/Api/ContentController.php
@@ -2,7 +2,6 @@
 
 namespace Opifer\ContentBundle\Controller\Api;
 
-use Elastica\JSON;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
